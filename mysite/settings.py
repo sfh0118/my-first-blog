e117256beff5c8042330130524c86c735834460e
sfh0118 <937125638sun@gmail.com>
2026-05-22 05:38:01 +0000

    template fix

--- mysite/settings.py
+++ mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-@i55fz)$trs7ui!3l@s-9o5zj^%5ycl%$!s&k*!y5%id1@8pq8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sfh0118.pythonanywhere.com']
 
 
 # Application definition
