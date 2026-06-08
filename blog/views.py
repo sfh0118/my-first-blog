57326b4b92217793746710661239892017610b05
sunfenghao <937125638sun@gmail.com>
2026-06-08 17:11:35 +0900

    1

--- blog/views.py
+++ blog/views.py
@@ -50,3 +50,5 @@ def post_edit(request, pk):
         form = PostForm(instance=post)
 
     return render(request, 'blog/post_edit.html', {'form': form})
+def js_test(request):
+    return render(request, 'blog/js_test.html', {})
