755d1fde4f91dd34ab2edc126f9a10f965dd25e3
sunfenghao <937125638sun@gmail.com>
2026-06-08 19:50:17 +0900

    1

--- blog/models.py
+++ blog/models.py
@@ -3,20 +3,25 @@ from django.utils import timezone
 
 class Post(models.Model):
 
-        title = models.CharField(max_length=200)
+    title = models.CharField(max_length=200)
 
-        text = models.TextField()
+    text = models.TextField()
 
-        created_date = models.DateTimeField(default=timezone.now)
+    created_date = models.DateTimeField(default=timezone.now)
 
-        published_date = models.DateTimeField(
-            blank=True,
-            null=True
-        )
+    published_date = models.DateTimeField(
+        blank=True,
+        null=True
+    )
 
-        def publish(self):
-            self.published_date = timezone.now()
-            self.save()
+    image = models.ImageField(
+        upload_to='intruder_image/%Y/%m/%d/',
+        default='intruder_image/default_error.png'
+    )
 
-        def __str__(self):
-            return self.title
+    def publish(self):
+        self.published_date = timezone.now()
+        self.save()
+
+    def __str__(self):
+        return self.title
